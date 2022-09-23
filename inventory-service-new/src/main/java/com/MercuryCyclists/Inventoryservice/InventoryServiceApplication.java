d1256a6508ddde7af403cc833a39cd4b038314fa
Isaac Farrell <isaac.farrell_115@live.com.au>
2022-09-23 16:21:17 +1000

    look up supplier by part

--- inventory-service-new/src/main/java/com/MercuryCyclists/Inventoryservice/InventoryServiceApplication.java
+++ inventory-service-new/src/main/java/com/MercuryCyclists/Inventoryservice/InventoryServiceApplication.java
@@ -3,8 +3,10 @@ package com.MercuryCyclists.Inventoryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
 import InventoryControllers.PartController;
 import InventoryControllers.ProductController;
@@ -21,6 +23,11 @@ import InventoryRepos.ProductRepository;
 @EntityScan(basePackageClasses = { Part.class, Product.class })
 public class InventoryServiceApplication {
 
+	@Bean    
+    RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
